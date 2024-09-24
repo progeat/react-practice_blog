e706a95d17f56beabbdfe8d4de957513397b48ca
Хохлов Андрей Анатольевич <A.Hohlov@transtelematica.ru>
2024-09-24 15:42:44 +0300

    first init

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-calendar"></i>
+			<div>123</div>
+		</Div>
+	);
+};
