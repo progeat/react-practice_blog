f5e90b212426112630550d94033dc041d0e79047
Khokhlov Andrey <progeat@mail.ru>
2024-09-30 16:34:44 +0300

    Создание страницы "Авторизация"

--- src/bff/get-user.js
+++ src/bff/get-user.js
@@ -3,5 +3,5 @@ import { getUsers } from './get-users';
 export const getUser = async (loginToFind) => {
 	const users = await getUsers();
 
-	return users.find(({ login }) => login === authLogin);
+	return users.find(({ login }) => login === loginToFind);
 };
---
+++ src/pages/authorization/authorization.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { server } from '../../bff';
+import styled from 'styled-components';
+
+const authFormSchema = yup.object().shape({
+	login: yup
+		.string()
+		.required('Заполните логин')
+		.matches(/^\w+$/, 'Неверно заполнен логин. Допускаются только буквы и цифры')
+		.min(3, 'Неверно заполнен логин. Минимум 3 символа')
+		.max(15, 'Неверно заполнен логин. Максимум 15 символов'),
+	password: yup
+		.string()
+		.required('Заполните пароль')
+		.matches(
+			/^[\w#%]+$/,
+			'Неверно заполнен пароль. Допускаются только буквы, цифры и знаки # %',
+		)
+		.min(6, 'Неверно заполнен пароль. Минимум 6 символов')
+		.max(30, 'Неверно заполнен пароль. Максимум 30 символов'),
+});
+
+const AuthorizationContainer = ({ className }) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			login: '',
+			password: '',
+		},
+		resolver: yupResolver(authFormSchema),
+	});
+
+	const [serverError, setServerError] = useState();
+
+	const onSubmit = ({ login, password }) => {
+		server.authorize(login, password).then(({ error, res }) => {
+			if (error) {
+				setServerError(`Ошибка запроса: ${error}`);
+			}
+		});
+	};
+
+	const formError = errors?.login?.message || errors?.password?.message;
+	const errorMessage = formError || serverError;
+
+	return (
+		<div className={className}>
+			<h2>Авторизация</h2>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input type="text" placeholder="Логин..." {...register('login')} />
+				<input
+					type="password"
+					placeholder="Пароль..."
+					{...register('password')}
+				/>
+				<button type="submit" disabled={!!formError}>
+					Войти
+				</button>
+				{errorMessage && <div>{errorMessage}</div>}
+			</form>
+		</div>
+	);
+};
+
+export const Authorization = styled(AuthorizationContainer)`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	& > form {
+		display: flex;
+		flex-direction: column;
+	}
+`;
