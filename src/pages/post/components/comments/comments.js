0a1536a29a0ac28f57e44870cac2612f56325c9e
Khokhlov Andrey <progeat@mail.ru>
2024-10-07 16:59:58 +0300

    Вёрстка страницы "Статья"

---
+++ src/pages/post/components/comments/comments.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const CommentsContainer = ({ className }) => {
+	return <div className={className}></div>;
+};
+
+export const Comments = styled(CommentsContainer)``;
